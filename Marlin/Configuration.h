206295f98631961d7b50566829f421be19757c6c
panhandle76 <38958263+panhandle76@users.noreply.github.com>
2020-03-13 21:09:28 -0400

    fixed panel type

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1530,7 +1530,7 @@
 //
 // Note: Usually sold with a white PCB.
 //
-//#define REPRAP_DISCOUNT_SMART_CONTROLLER
+#define REPRAP_DISCOUNT_SMART_CONTROLLER
 
 //
 // ULTIMAKER Controller.
@@ -1540,7 +1540,7 @@
 //
 // ULTIPANEL as seen on Thingiverse.
 //
-#define ULTIPANEL
+//#define ULTIPANEL
 
 //
 // PanelOne from T3P3 (via RAMPS 1.4 AUX2/AUX3)
